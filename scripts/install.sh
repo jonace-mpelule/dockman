8a6cf7e494c45891d6ddf32c6f8253e7532516bc
Jonace Mpelule <me@jonacempelule.com>
2026-02-03 14:48:14 +0200

    Update:Script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 set -e
 
-VERSION="v1.0.1"
+VERSION="v1.0.2"
+
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
-case "$ARCH" in
-  x86_64) ARCH="amd64" ;;
-  aarch64) ARCH="arm64" ;;
-  *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+case "$OS" in
+  linux)
+    case "$ARCH" in
+      x86_64) ARCH="amd64" ;;
+      aarch64) ARCH="arm64" ;;
+      *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+    esac
+    ;;
+  darwin)
+    case "$ARCH" in
+      x86_64) ARCH="amd64" ;;
+      arm64) ARCH="arm64" ;;
+      *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+    esac
+    ;;
+  *)
+    echo "Unsupported OS: $OS"; exit 1
+    ;;
 esac
 
 URL="https://github.com/jonace-mpelule/dockman/releases/download/$VERSION/dockman-$OS-$ARCH"
