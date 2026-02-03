4eb95112ce710bf6ad0198c5702e7465f9f9411b
Jonace Mpelule <me@jonacempelule.com>
2026-02-03 14:27:01 +0200

    Update: New Release

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,13 +5,15 @@ VERSION="v1.0.0"
 OS=$(uname | tr '[:upper:]' '[:lower:]')
 ARCH=$(uname -m)
 
-# Map uname architecture to Go arch
-if [ "$ARCH" = "x86_64" ]; then
-    ARCH="amd64"
-fi
+case "$ARCH" in
+  x86_64) ARCH="amd64" ;;
+  aarch64) ARCH="arm64" ;;
+  *) echo "Unsupported architecture: $ARCH"; exit 1 ;;
+esac
 
 URL="https://github.com/jonace-mpelule/dockman/releases/download/$VERSION/dockman-$OS-$ARCH"
+echo "Downloading $URL..."
 curl -L "$URL" -o /usr/local/bin/dockman
 chmod +x /usr/local/bin/dockman
 
-echo "Installed dockman $VERSION!"
+echo "dockman $VERSION installed successfully!"
